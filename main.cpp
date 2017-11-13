ed730d7622dd4ae7f05f13d57a760f97252120af
DolanL <32385280+DolanL@users.noreply.github.com>
2017-11-13 17:48:15 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,46 +1,21 @@
 #include <iostream>
-#include <string>
-#include <limits>
 using namespace std;
-int main() {
-  string length;
-  string array;
-  string integer_str;
-  string result;
-  int length_int;
-  int counter = 0;
-  int k = 0;
-  int z = 0;
-  int array1[100];
-  cin >> length;
-  cin.ignore(numeric_limits<streamsize>::max(), '\n');
-  getline(cin, array);
-  length_int = atoi(length.c_str());
-  for (int i = 0; i < array.length(); i++) {
-    if (array[i] == ' ') counter++;
-  }
-  if (length_int != (counter + 1))
-    cout << " An error has occurred while reading" << endl;
-  else {
-    for (int i = 0; i < (array.length() / 2); i++) {
-      swap(array[i], array[(array.length()) - i - 1]);
-    }
-    while (true) {
-      for (int i = z; i < array.length(); i++) {
-        if (array[i] == ' ') {
-          z++;
-          break;
-        }
-        integer_str = integer_str + array[i];
-        z++;
-      }
-      if (integer_str == "") break;
-      for (int j = 0; j < ((integer_str.length()) / 2); j++) {
-        swap(integer_str[j], integer_str[integer_str.length() - j - 1]);
-      }
-      result = result + integer_str + " ";
-      integer_str = "";
-    }
-    cout << result << endl;
-  }
+int main(int argc, char* argv[]){
+int a, i;
+cin » a;
+if (a <= 0){
+cout « "An error has occurred while reading input data" « endl;
+return 0; 
+}
+int *k = new int[a];
+for (i = 0; i < a; i++) {
+cin » k[i];
+}
+for (i = 0; i < a / 2; i++) {
+swap(k[i], k[a - i - 1]);
+}
+for (i = 0; i < a; i++) {
+cout « k[i] « " ";
+}
+return 0;
 }
