18b288d47a501fffd5dce082f2c920e557dce2da
DolanL <32385280+DolanL@users.noreply.github.com>
2017-11-13 17:48:48 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,20 +2,20 @@
 using namespace std;
 int main(int argc, char* argv[]){
 int a, i;
-cin » a;
+cin >> a;
 if (a <= 0){
-cout « "An error has occurred while reading input data" « endl;
+cout << "An error has occurred while reading input data" << endl;
 return 0; 
 }
 int *k = new int[a];
 for (i = 0; i < a; i++) {
-cin » k[i];
+cin >> k[i];
 }
 for (i = 0; i < a / 2; i++) {
 swap(k[i], k[a - i - 1]);
 }
 for (i = 0; i < a; i++) {
-cout « k[i] « " ";
+cout << k[i] << " ";
 }
 return 0;
 }
